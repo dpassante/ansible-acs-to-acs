141b489969a7d6e339264986eee5a04055207058
David Passante <david.passante@orange.com>
2020-03-11 18:18:39 +0100

    remove network tags patch as it is not mandatory

--- ansible_prior_2.9_cs_network_tags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ansible_path=$(ansible --version |sed -n "s@.*ansible python module location = \(/usr/[a-z/]*lib/python[23].[0-9]/dist-packages/ansible\)@\1@p")
-patch_file=$(readlink -f patch/ansible_cs_network_tags.patch)
-
-pushd ${ansible_path}
-patch -i ${patch_file} -p1
-popd
